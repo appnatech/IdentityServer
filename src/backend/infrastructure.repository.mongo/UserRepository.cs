864f94aa622b7051360e0512188493a362aab568
boumipour <majid.boumipour@gmail.com>
2021-01-11 17:33:50 +0330

    insert user command

--- src/backend/infrastructure.repository.mongo/UserRepository.cs
+++ src/backend/infrastructure.repository.mongo/UserRepository.cs
@@ -1,19 +1,38 @@
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using domain.core.models;
 using domain.core.repositories;
+using infrastructure.repository.mongo.config;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace infrastructure.repository.mongo
 {
     public class UserMongoRepository : IUserRepository
     {
-        public Task<User> AddAsync(User user)
+        private const string IdentityResourceCollectionName = "Users";
+        private readonly IMongoCollection<User> _users;
+
+        public UserMongoRepository(IOptions<MongoDataBaseConfigurations> config)
         {
-            throw new System.NotImplementedException();
+            var client = new MongoClient(config.Value.ConnectionString);
+            var database = client.GetDatabase(config.Value.DatabaseName);
+
+            _users = database.GetCollection<User>(IdentityResourceCollectionName);
         }
 
-        public Task<User> GetBySubjectIdAsync(string subjectId)
+
+        public async Task<User> AddAsync(User user)
         {
-            throw new System.NotImplementedException();
+            await _users.InsertOneAsync(user);
+            return user;
+        }
+
+        public async Task<User> GetBySubjectIdAsync(string subjectId)
+        {
+            IAsyncCursor<User> user = await _users.FindAsync(w => w.SubjectId == subjectId);
+            return user.SingleOrDefault();
         }
 
         public Task<User> GetByUsernameAndPasswordAsync(string username, string password)
@@ -22,11 +41,6 @@ namespace infrastructure.repository.mongo
         }
 
         public Task<User> GetByUsernameAsync(string userName)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<bool> ValidateCredentialsAsync(string username, string password, out User user)
         {
             throw new System.NotImplementedException();
         }
